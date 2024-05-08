24c12b08c38ee0dbdbef125b138f2c953004eae2
Reek12138 <2894487027@qq.com>
2024-05-08 20:02:07 +0800

    rvo update

---
+++ compile_rvo.sh
@@ -0,0 +1,3 @@
+catkin_make --source Modules/common/msgs --build build/msgs
+catkin_make --source Modules/ground_station --build build/ground_station
+catkin_make --source Modules/rvo_ros --build build/rvo_ros
